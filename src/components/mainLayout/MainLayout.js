8d0f9edaee81c15b4e0d136923a768b553aa50f6
Juan Saglione <juansaglione123@gmail.com>
2023-10-24 14:47:13 -0300

    add footer

--- src/components/mainLayout/MainLayout.js
+++ src/components/mainLayout/MainLayout.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "../header/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "../footer/Footer";
 
 const MainLayout = () => {
-    return (
-        <>
-            <Header />
-            <Outlet />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default MainLayout;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,61 +10,61 @@ import { ThemeContext } from "../../services/theme/theme.context";
 import { Divider } from "@nextui-org/react";
 
 const Home = () => {
-    const [newProducts, setNewProducts] = useState([]);
-    const { theme } = useContext(ThemeContext);
+  const [newProducts, setNewProducts] = useState([]);
+  const { theme } = useContext(ThemeContext);
 
-    useEffect(() => {
-        fetch("http://localhost:8080/products/new", {
-            headers: {
-                Accept: "application/json",
-            },
-        })
-            .then((response) => response.json())
-            .then((products) => {
-                setNewProducts(products);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
-    const newProductsList = newProducts.map((product) => (
-        <ProductCard
-            key={product.id}
-            id={product.id}
-            price={product.price}
-            discount={product.discount}
-            brand={product.brand}
-            productName={product.productName}
-            image="https://acdn.mitiendanube.com/stores/219/431/products/824f0f4a-d566-4f40-812e-d2c7d9c13ac5-1910a0efe81b0c4db216935215790830-480-0.webp"
-        />
-    ));
+  useEffect(() => {
+    fetch("http://localhost:8080/products/new", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((products) => {
+        setNewProducts(products);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  const newProductsList = newProducts.map((product) => (
+    <ProductCard
+      key={product.id}
+      id={product.id}
+      price={product.price}
+      discount={product.discount}
+      brand={product.brand}
+      productName={product.productName}
+      image="https://acdn.mitiendanube.com/stores/219/431/products/824f0f4a-d566-4f40-812e-d2c7d9c13ac5-1910a0efe81b0c4db216935215790830-480-0.webp"
+    />
+  ));
 
-    return (
-        <div
-            class={
-                theme === "dark"
-                    ? "h-container h-container-dark"
-                    : "h-container"
-            }
-        >
-            <HomeCarousel />
-            <Divider class={theme === "dark" ? "dark" : "light"} />
-            <section class="info-section">
-                <InfoBaner />
-            </section>
-            <Divider class={theme === "dark" ? "dark" : "light"} />
-            {newProductsList.length > 0 ? (
-                <section class="trend-products">
-                    <h2 class="new-in">New In</h2>
-                    <div class="trend-products-container">
-                        <CardSlider>{newProductsList}</CardSlider>
-                    </div>
-                </section>
-            ) : (
-                <Spinner1 />
-            )}
-        </div>
-    );
+  return (
+    <div
+      class={
+        theme === "dark"
+          ? "h-container h-container-dark pb-4"
+          : "h-container pb-4"
+      }
+    >
+      <HomeCarousel />
+      <Divider class={theme === "dark" ? "dark" : "light"} />
+      <section class="info-section">
+        <InfoBaner />
+      </section>
+      <Divider class={theme === "dark" ? "dark" : "light"} />
+      {newProductsList.length > 0 ? (
+        <section class="trend-products">
+          <h2 class="new-in">New In</h2>
+          <div class="trend-products-container">
+            <CardSlider>{newProductsList}</CardSlider>
+          </div>
+        </section>
+      ) : (
+        <Spinner1 />
+      )}
+    </div>
+  );
 };
 
 export default Home;
