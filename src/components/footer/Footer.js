660c1f283f2139ffcf6fd3248bf07f63ed274232
Juan Saglione <juansaglione123@gmail.com>
2023-11-05 20:44:16 -0300

    fix diseño dividers/footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,109 +5,90 @@ import "./Footer.css";
 import { Divider } from "@nextui-org/react";
 
 const Footer = () => {
-    const { theme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
 
-    return (
-        <div>
-            <div
-                className={
-                    theme === "dark" ? "fot-dark-theme" : "fot-light-theme"
-                }
-            >
-                <Divider
-                    className={
-                        theme === "dark" ? "dark dark-fot-divider" : "light"
-                    }
-                />
-                <footer className="pt-3">
-                    <div class="container-fluid text-center text-md-left">
-                        <div className="row">
-                            <div className="col-md-6 pt-2 pb-4">
-                                <h6 className="text-uppercase">Contactános</h6>
-                                <a
-                                    className="text-cyan-500"
-                                    href="mailto:juansaglione123@gmail.com"
-                                >
-                                    Mandános un mail
-                                </a>
-                                <li className="list-unstyled">
-                                    <a
-                                        className={
-                                            theme === "dark"
-                                                ? "text-gray-400 no-underline hover:underline"
-                                                : "text-gray-600 no-underline hover:underline"
-                                        }
-                                        href="https://maps.app.goo.gl/hvPamjLHT7PCx4U57"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        Rosario, Santa Fe, AR.
-                                    </a>
-                                </li>
-                            </div>
-                            <hr className="clearfix w-100 d-md-none" />
-                            <div className="col-md-5 pt-2">
-                                <h6 className="text-uppercase">
-                                    Encontrános en Instagram
-                                </h6>
-                                <ul className="list-unstyled">
-                                    <li className="py-1">
-                                        <a
-                                            className="fot-social"
-                                            target="_blank"
-                                            href="https://www.instagram.com/nicointelangelo/"
-                                            rel="noreferrer"
-                                        >
-                                            Nico
-                                        </a>
-                                    </li>
-                                    <li className="py-1">
-                                        <a
-                                            className="fot-social"
-                                            rel="noreferrer"
-                                            href="https://www.instagram.com/juansaglione/"
-                                            target="_blank"
-                                        >
-                                            Juan
-                                        </a>
-                                    </li>
-                                    <li className="py-1">
-                                        <a
-                                            className="fot-social"
-                                            rel="noreferrer"
-                                            href="https://www.instagram.com/mateo_monti/"
-                                            target="_blank"
-                                        >
-                                            Mateo
-                                        </a>
-                                    </li>
-                                    <li className="py-1">
-                                        <a
-                                            className="fot-social"
-                                            rel="noreferrer"
-                                            href="https://www.instagram.com/santiagocrucianelli/"
-                                            target="_blank"
-                                        >
-                                            Santi
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="footer-copyright text-center py-3">
-                            © 2023 Copyright:
-                            <a
-                                className="fot-social"
-                                href="https://campusvirtual.austral.edu.ar/"
-                            >
-                                ‎ CampusAustral.com
-                            </a>
-                        </div>
-                    </div>
-                </footer>
+  return (
+    <div className={theme === "dark" ? "dark fot-dark-theme" : "light"}>
+      <Divider className="my-0 fot-divider" />
+      <footer className="pt-3">
+        <div className="container-fluid text-center text-md-left">
+          <div className="row ">
+            <div className="col-md-6 pb-3">
+              <h6 className="text-uppercase py-3">Contactános</h6>
+              <a className="fot-social" href="mailto:juansaglione123@gmail.com">
+                Mandános un mail
+              </a>
+              <li className="list-unstyled pt-2">
+                <a
+                  className="text-neutral-500 no-underline"
+                  href="https://maps.app.goo.gl/hvPamjLHT7PCx4U57"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Rosario, Santa Fe, AR.
+                </a>
+              </li>
             </div>
+            <Divider className="d-md-none fot-divider my-3" />
+            <div className="col-md-5">
+              <h6 className="text-uppercase py-3">Encontrános en Instagram</h6>
+              <ul className="list-unstyled">
+                <li className="pb-1">
+                  <a
+                    className="fot-social"
+                    target="_blank"
+                    href="https://www.instagram.com/nicointelangelo/"
+                    rel="noreferrer"
+                  >
+                    @nicointelangelo
+                  </a>
+                </li>
+                <li className="py-1">
+                  <a
+                    className="fot-social"
+                    rel="noreferrer"
+                    href="https://www.instagram.com/juansaglione/"
+                    target="_blank"
+                  >
+                    @juansaglione
+                  </a>
+                </li>
+                <li className="py-1">
+                  <a
+                    className="fot-social"
+                    rel="noreferrer"
+                    href="https://www.instagram.com/mateo_monti/"
+                    target="_blank"
+                  >
+                    @mateo_monti
+                  </a>
+                </li>
+                <li className="py-1">
+                  <a
+                    className="fot-social"
+                    rel="noreferrer"
+                    href="https://www.instagram.com/santiagocrucianelli/"
+                    target="_blank"
+                  >
+                    @santiagocrucianelli
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="footer-copyright text-center py-3">
+            © 2023 Copyright:
+            <a
+              className="text-cyan-500 no-underline"
+              href="https://campusvirtual.austral.edu.ar/"
+            >
+              ‎ CampusAustral.com
+            </a>
+          </div>
         </div>
-    );
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
